66195d5be10495457aae86274ff38a14effd1807
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-01-22 22:55:25 -0300

    Exercises 4.18.-4.19.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,23 +18,29 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes } = request.body
+  const token = getTokenFrom(request)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
   if (!title || !url) {
     return response.status(400).json({ error: 'incomplete properties' })
   }
-  const allUsers = await User.find({})
-  const index = Math.floor(Math.random() * allUsers.length)
-  const userFound = allUsers[index]
+
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title,
     author,
     url,
     likes,
-    user: userFound._id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
-  userFound.blogs = userFound.blogs.concat(savedBlog._id)
-  await userFound.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
